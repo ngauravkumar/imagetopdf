7089d26e86bf94a0d387ee6907583df02ce2ab7b
gaurav kumar <gaurav.kumar@bvceservices.com>
2022-02-21 21:28:27 +0530

    test

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.use(express.static("public"));
+app.use('/front',express.static(__dirname +'/front'));
 
 var list = ""
 
