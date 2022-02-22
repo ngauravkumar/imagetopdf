bb0bbdd253febb078c991ab682648c2dd0b0d3ed
gaurav kumar <gaurav.kumar@bvceservices.com>
2022-02-22 20:31:51 +0530

    test

--- index.js
+++ index.js
@@ -93,6 +93,7 @@ app.post('/merge', upload.array('files', 100), (req, res) => {
         });
 
         // fs.unlinkSync(outputFilePath)
+        res.redirect('/valid=' + outputFilePath);
         console.log(outputFilePath);
         //res.render('index', { message: req.flash('PDF Generate Successfully, And Download Automatically..') });
       })
