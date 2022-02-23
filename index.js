e600a3d453185ac7592dbbea3692750511cdc03a
gaurav kumar <gaurav.kumar@bvceservices.com>
2022-02-23 15:20:42 +0530

    test

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ app.post('/merge', upload.array('files', 100), (req, res) => {
 
     console.log(list)
 
-    exec(`magick convert ${list} -gravity center -quality ${range_value} -page ${page_size} ${outputFilePath}`, (err, stdout, stderr) => {
+    exec(`magick convert ${list} -quality ${range_value} -page ${page_size} -gravity center ${outputFilePath}`, (err, stdout, stderr) => {
       if (err) throw err
 
       res.download(outputFilePath, (err) => {
